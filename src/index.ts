17896fdc1cfc6051b175a4fec8a5c3a7af2292d6
hungnguyen18 <hungtrewq3214@gmail.com>
2026-03-30 00:01:51 +0700

    Add public API exports and verify build output

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+export {
+  generate,
+  generateFullName,
+  generateMany,
+  generateManyFullNames,
+} from "./generator";
+
+export {
+  EGender,
+  ERegion,
+  EEra,
+  EMeaningCategory,
+  type TGenerateOptions,
+  type INameResult,
+  type TWeightedEntry,
+  type TGivenNameEntry,
+} from "./types";
